1094cea592d986b3276f453308f1fb82b904d43b
Guido Fanfani <guido.fanfani7@gmail.com>
2019-10-14 07:14:02 +0200

    Create GFPercSpinner.h

---
+++ GFPercSpinner.h
@@ -0,0 +1,55 @@
+//
+//  GFSimpleSpinner.h
+//  // Fanfa
+//
+//  Created by Guido Fanfani on 22/09/19.
+//  Copyright © 2019 Fanfa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFPercSpinner : UIView
+
+/**
+ Set color of spinner
+ @param color of spinner stroke. default: grayColor
+ */
+- (void)setStrokeColor:(UIColor*)color;
+
+/**
+ Set spinner stroke
+ @param width spinner stroke. default: 3
+ */
+- (void)setStrokeWidth:(float)width;
+
+/**
+ Update spinner's frame
+ @param frame of spinner
+ */
+- (void)updateFrame:(CGRect)frame;
+
+/**
+ set current progress = 0
+ */
+- (void)reset;
+
+/**
+ Method to update current progress
+ @param perc set current progress. Value is between [0,1]
+ */
+- (void)updateProgress:(float)perc;
+
+/**
+ Get current progress
+ @return Float that rappresent current progress
+ */
+- (float)currentProgress;
+
+
+- (void)startAnimation;
+
+- (void)stopAnimation;
+
+- (bool)isAnimation;
+
+@end
